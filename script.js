814a8bfff15deb91180e9e9771dcef59dacafc9e
Sean Lazaro <seanchristienlazaro@gmail.com>
2015-05-10 22:38:55 -0400

    Added crossing off function

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $("#addItemBtn").click(function(){
-    $("#toDoList").append('<li>' + $("input[name=addedItem]").val() + '</li>');
+    $("#toDoList").append("<li>" + $("input[name=addedItem]").val() + "</li>");
   });
+ $(document).on("click", "#toDoList li", function(){
+  	$(this).css("text-decoration", "line-through");
+  });
+  /*$(document).on("click", "", function(){
+
+  });*/
 });
