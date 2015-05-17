c2ce8a532e1096175579c9dd2b7e634552a36ff0
Sean Lazaro <seanchristienlazaro@gmail.com>
2015-05-17 00:52:43 -0400

    Added sortability

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ $(document).ready(function(){
 	$("#addItemBtn").click(function(){
 		var entryTxt = "<li>" + $("input[name=addedItem]").val() + "</li>";
     
-    var dueInput = "<input type='text' class='dueInput'>";
+    var dueInput_disp = "<input type='text' class='dueInput_disp'>";
+    var dueInput_sort = "<input type='text' class='dueInput_sort'>";
     var addTag = "<div class='addTag'>";
     addTag += "<input type='text' class='tagInput'>";
     addTag += "<span class='glyphicon glyphicon-plus addTagBtn'></span>";
     addTag += "</div>";
 
-    var entryDesc = "<div class='entryDesc'>" + dueInput + addTag + "</div>";
+    var entryDesc = "<div class='entryDesc'>" + dueInput_disp + dueInput_sort + addTag + "</div>";
 
     var entryMain = "<div class='entryMain'>" + entryTxt + entryDesc + "</div>";
 
@@ -25,7 +26,7 @@ $(document).ready(function(){
     $("#toDoList").append(listEntry);
     $("input[name=addedItem]").val("");
 
-    $(".dueInput").hide();
+    $(".dueInput_disp").hide();
     $(".addTag").hide();
 
     $(".listCtrl").show();
@@ -45,13 +46,14 @@ $(document).ready(function(){
   });
 
   $(document).on("click", ".dueBtn", function(){
-
-    $(this).parent().parent().find(".dueInput").datepicker({
+    $(this).parent().parent().find(".dueInput_disp").datepicker({
       onSelect: function(input, inst){
       $(this).parent().parent().find(".entryDesc").prepend("<div class='tag dueTag'>" + input + "</div>");
       $(this).parent().parent().parent().addClass("due");
       },
-      dateFormat: "MM dd, yy"
+      dateFormat: "MM dd, yy",
+      altField: $(this).parent().parent().find(".dueInput_sort"),
+      altFormat: "yymmdd"
     });
     
     if($(this).parent().parent().hasClass("due")){
@@ -59,7 +61,7 @@ $(document).ready(function(){
       $(this).parent().parent().find(".dueTag").remove();
     }
     else{
-      $(this).parent().parent().find(".dueInput").show().focus().hide();
+      $(this).parent().parent().find(".dueInput_disp").show().focus().hide();
     }
   });
 
@@ -81,6 +83,46 @@ $(document).ready(function(){
     }
     $(this).remove();
   });  
+
+  $("#sortBtn").click(function(){
+    var listEntries = $(".listEntry");
+    listEntries.sort(function(entry1, entry2){
+      var txt1 = $(entry1).find("li").text();
+      var txt2 = $(entry2).find("li").text();
+      if(!$(entry1).hasClass("due") && !$(entry2).hasClass("due")){
+        if(txt1 > txt2){
+          return 1;
+        } else if(txt2 > txt1){
+          return -1;
+        } else{
+          return 0;
+        }
+      }
+      else if(!$(entry1).hasClass("due")){
+        return 1;
+      }
+      else if(!$(entry2).hasClass("due")){
+        return -1;
+      }
+
+      var date1 = Number($(entry1).find(".dueInput_sort").val());
+      var date2 = Number($(entry2).find(".dueInput_sort").val());
+      if(date1 > date2){
+        return 1;
+      } else if(date2 > date1){
+        return -1;
+      } else{
+        if(txt1 > txt2){
+          return 1;
+        } else if(txt2 > txt1){
+          return -1;
+        } else{
+          return 0;
+        }
+      }
+    });
+    listEntries.detach().appendTo("#toDoList");
+  });
 
   $("#filterBtn").click(function(){
     var filter = $("input[name=filterInput]").val().toLowerCase();
